40cc642fe7439f06dba4ffa3922f05b56ddc8580
KMdatagithub <khaledelrawy9000@yahoo.com>
2020-01-18 16:41:35 +0200

    Initial commit

---
+++ Debug/main.d
@@ -0,0 +1,19 @@
+main.d main.o: .././main.c .././registers.h .././std_types.h \
+ .././softwareDelay.h .././gpio.h .././led.h .././ledConfig.h \
+ .././pushButton.h .././pushButtonConfig.h
+
+.././registers.h:
+
+.././std_types.h:
+
+.././softwareDelay.h:
+
+.././gpio.h:
+
+.././led.h:
+
+.././ledConfig.h:
+
+.././pushButton.h:
+
+.././pushButtonConfig.h:
